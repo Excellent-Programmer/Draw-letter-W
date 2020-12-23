b0b9f6fc9fdc9ba79292a4dba0398253c8bf224e
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-23 19:15:57 +0200

    Add files via upload

---
+++ letter W/letter W/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letter_W
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (((column == 1 || column == 5) && row < 6) || ((row == 5 || row == 4) && column == 3) || (row == 6 && (column == 2 || column == 4)))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Read();
+        }
+    }
+}
